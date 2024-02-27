5e6fcc30122b7149c927a8a8651c25c64f149ad9
Koromonnnnnnnn <764363@dpsk12.net>
2024-02-27 09:08:05 -0700

    Create main.py

---
+++ Workspace/main.py
@@ -0,0 +1,46 @@
+import pygame
+import random
+
+pygame.init()
+
+# Constants
+WIDTH, HEIGHT = 700, 500
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (200, 200, 100)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Invaders")
+clock = pygame.time.Clock()
+
+# Game Variables
+xpos = 400
+ypos = 750
+moveLeft = False
+
+while not game_over:
+
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+            
+    # Physics section
+    
+    if moveLeft == True:
+        vx =- 3
+    else:
+        vx = 0
+        
+    xpos += vx
+    
+    # Render Section
+    
+    screen.fill((BLACK)) # wipe screen
+    pygame.draw.rect(screen, (GREEN), (400, 750, 60, 20)) # draw player
+    pygame.display.flip()
+
+pygame.quit()
