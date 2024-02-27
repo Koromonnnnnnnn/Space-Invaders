d6bc605756f1135ace6f2a39e918257c86669007
Koromonnnnnnnn <764363@dpsk12.net>
2024-02-27 10:01:14 -0700

    Update main.py

--- Workspace/main.py
+++ Workspace/main.py
@@ -4,23 +4,48 @@ import random
 pygame.init()
 
 # Constants
-WIDTH, HEIGHT = 700, 500
+width, height = 800, 800
+alienWidth, alienHeight = 40, 40
+alienRows = 4
+AlienCols = 12
 
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-GREEN = (200, 200, 100)
+GREEN = (102, 255, 0)
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Space Invaders")
 clock = pygame.time.Clock()
 gameOver = False
 
 # Game Variables
-xpos = 800
-ypos = 800
+xpos = 365
+ypos = 750
+timer = 0
 moveLeft = False
 moveRight = False
+
+
+class Alien:
+    def __init__(self, xpos, ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.isDead = False
+
+    def draw(self):
+        pygame.draw.rect(screen, (WHITE), (self.xpos,
+                         self.ypos, alienWidth, alienHeight))
+
+    def move(self):
+        if timer % 100 == 0:
+            self.xpos += 50
+
+
+armada = []
+for row in range(alienRows):
+    for col in range(AlienCols):
+        armada.append(Alien(col*60+50, row*50+50))
 
 while not gameOver:
 
@@ -29,32 +54,42 @@ while not gameOver:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameOver = True
-            
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT]:
         moveRight = True
+    else:
+        moveRight = False
+
     if keys[pygame.K_LEFT]:
         moveLeft = True
-            
+    else:
+        moveLeft = False
+
     # Physics section
-    
-    if moveLeft == True:
-        vx =- 3
-    else:
-        vx = 0
-        
-    if moveRight == True:
-        vy += 3
-    else:
-        vy = 0
-        
+    vx = 0
+    vy = 0
+
+    if moveLeft:
+        vx = -3
+
+    if moveRight:
+        vx = 3
+
     xpos += vx
     ypos += vy
-    
+
     # Render Section
-    
-    screen.fill((BLACK)) # wipe screen
-    pygame.draw.rect(screen, (GREEN), (400, 750, 60, 20)) # draw player
+
+    screen.fill((BLACK))  # wipe screen
+
+    for i in range(len(armada)):
+        armada[i].draw()
+
+    pygame.draw.rect(screen, (GREEN), (xpos, ypos, 60, 20))  # draw player
+    pygame.draw.rect(
+        screen, (GREEN), (xpos+20, ypos-10, 20, 10))  # draw player
+
     pygame.display.flip()
 
 pygame.quit()
